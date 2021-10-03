499d600916a1b4dd170f1d7858b2e89b1e5b7016
Krzysiek-89 <80069899+Krzysiek-89@users.noreply.github.com>
2021-10-03 12:03:05 +0200

    add to app

---
+++ Project/my-app/src/Data/AboutMe.js
@@ -0,0 +1,9 @@
+const AboutMe = {
+  name: "Krzysztof",
+  lastName: "Pinkiewicz",
+  age: "32",
+  description:
+    "Sometimes i Heve to screw up to understand what's  wrong with me. Thanks for the  second chance",
+};
+
+export default AboutMe;
---
+++ Project/my-app/src/Data/CardsInfo.js
@@ -0,0 +1,46 @@
+const CardsInfo = [
+  {
+    id: 1,
+    title: "O Mnie",
+    add: "/About",
+    result: true,
+    description: "Kliknij jeśli chcesz dowiedzieć się więcej",
+  },
+
+  {
+    id: 2,
+    title: "Stoper",
+    add: "/Stopwatch",
+    description: "Kliknij jeśli chcesz dowiedzieć się więcej",
+  },
+
+  {
+    id: 3,
+    title: "Licznik",
+    add: "/Counter",
+    description: "Kliknij jeśli chcesz dowiedzieć się więcej",
+  },
+
+  {
+    id: 4,
+    title: "Lista Postaci",
+    add: "/List",
+    description: "kliknij jeśli chcesz dowiedzieć się więcej",
+  },
+
+  {
+    id: 5,
+    title: "Logowanie",
+    add: "/Login",
+    description: "Zaloguj się",
+  },
+
+  {
+    id: 6,
+    title: "Rejestracja",
+    add: "/Registration",
+    description: "Zarejestruj się",
+  },
+];
+
+export default CardsInfo;
