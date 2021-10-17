e09e8c2da683c8d644a99c308cc0ef547ab32324
Krzysiek-89 <80069899+Krzysiek-89@users.noreply.github.com>
2021-10-17 11:12:29 +0200

    addpoke

---
+++ Poke-app/poke-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+import CardsInfo from "./Data/CardsInfo";
+
+function App() {
+  console.log();
+
+  return <div className="App"></div>;
+}
+
+export default App;
--- Project/my-app/src/Data/AboutMe.js
+++
@@ -1,9 +0,0 @@
-const AboutMe = {
-  name: "Krzysztof",
-  lastName: "Pinkiewicz",
-  age: "32",
-  description:
-    "Sometimes i Heve to screw up to understand what's  wrong with me. Thanks for the  second chance",
-};
-
-export default AboutMe;
